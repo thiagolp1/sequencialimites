6923fdd89fadda0acb444e93c420f2e088dcf35a
Thiago <Thiagolopespereiralp1@gmail.com>
2022-06-06 19:23:37 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int inicio, fim;
+string mensagem = "";
+
+Console.Write("Primeiro número:");
+inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ultimo número:");
+fim = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    if (fim <= inicio)
+    {
+        throw new Exception("O segundo número não pode ser menor que o primeiro");
+    }
+
+    while (fim >= inicio)
+    {
+        mensagem += $"{inicio} ";
+        inicio +=1;
+    }
+
+    Console.WriteLine($"{mensagem}");
+
+}
+
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
